25987592177e87f24f9c1bf4979bc7d5bd7be0bb
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2026-07-01 23:10:22 +0200

    chore(codegen): update code generation command to track latest

--- unifi/codegen.go
+++ unifi/codegen.go
@@ -1,7 +1,6 @@
 package unifi
 
-// 10.2.97 --> 10.3.58 --> 10.4.57 --> 10.5.54
 // This will generate the *.generated.go files in this package for the specified
 // client controller version. The positional argument is the Official-API spec
 // version; -legacy-version pins the Internal/legacy controller resources.
-//go:generate go run ../codegen/ -version-base-dir=../codegen/ -legacy-version=9.5.21 10.4.57
+//go:generate go run ../codegen/ -version-base-dir=../codegen/ -legacy-version=9.5.21 latest
